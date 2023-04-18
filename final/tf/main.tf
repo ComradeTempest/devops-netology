5994cb5ba6b8428822520a257ceeab42294a7657
ComradeTempest <98019531+ComradeTempest@users.noreply.github.com>
2023-04-18 13:44:06 +0300

    Update main.tf

--- final/tf/main.tf
+++ final/tf/main.tf
@@ -11,15 +11,15 @@ terraform {
     bucket     = "final-tf-storage"
     region     = "ru-central1"
     key        = "./terraform.tfstate"
-    access_key = "YCAJEHr7wXiARHd64LhrPc0U2"
-    secret_key = "YCNy-ltLnlFjUXbM-daWq5dz7XnA5SLbALJesLBP"
+    access_key = ""
+    secret_key = ""
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
 provider "yandex" {
-  token     = "AQAAAABf4qNTAATuwSozT1SSNUrSplnNk943h48"
-  cloud_id  = "b1gpd73ealtdsf16uniu"
-  folder_id = "b1gvgevkn6e5fincv69b"
+  token     = ""
+  cloud_id  = ""
+  folder_id = ""
   zone      = "ru-central1-a"
 }
